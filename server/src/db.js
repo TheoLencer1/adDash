c4047bfe4c417aa1460aad2b7260de715a193f67
Bubu Adico⚡️ <adikobubu@gmail.com>
2024-06-13 02:53:43 +0000

    Backend Server initial

--- server/src/db.js
+++ server/src/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_URL);
+		await mongoose.connect(process.env.MONGO_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 		console.log("MongoDB is connected successfully");
 	} catch (error) {
 		console.log({ message: error.message });
